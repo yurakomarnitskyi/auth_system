9aa7db05a25481b16742b27dc534242f1759fdd7
vladislava <vlada.kryazh@gmail.com>
2023-12-20 16:20:46 +0200

    added buttons

--- project-auth/product_questions/signals.py
+++ project-auth/product_questions/signals.py
@@ -1,7 +1,8 @@
 import asyncio
 import os
 
-from aiogram import Bot
+from aiogram import Bot, types
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -12,8 +13,16 @@ from product_questions.models import Comment
 
 
 async def send_update_info(text, bot):
+    """Send Telegram message to a specific chat with given text and buttons 'Answer question', 'Delete question'"""
+    markup = InlineKeyboardBuilder()
+
+    answer = types.InlineKeyboardButton(text='\U00002705 Answer question', callback_data="answer_question")
+    markup.add(answer)
+    delete = types.InlineKeyboardButton(text='\U0000274C Delete question', callback_data="delete_question")
+    markup.add(delete)
+
     async with bot.session:
-        await bot.send_message(340678127, text)
+        await bot.send_message(340678127, text, reply_markup=markup.as_markup())
 
 
 @receiver(post_save, sender=Comment)
--- project-auth/product_questions/telegram/bot.py
+++ project-auth/product_questions/telegram/bot.py
@@ -4,17 +4,23 @@ import os
 import re
 
 from aiogram import Bot, Dispatcher, F, types
+from aiogram.filters import StateFilter
 from aiogram.filters.command import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import KeyboardButton, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 from asgiref.sync import sync_to_async
 
 import django
+from django.core.exceptions import ObjectDoesNotExist
 
 from dotenv import load_dotenv
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "auth_system.settings")
 django.setup()
 
+from accounts.models import UserAccount
 from product_questions.models import Comment
 
 
@@ -23,6 +29,14 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+
+class ConfirmCommentDeletion(StatesGroup):
+    confirm_deletion = State()
+
+
+class SendAnswer(StatesGroup):
+    sending_answer = State()
 
 
 @dp.message(Command("start"))
@@ -34,8 +48,88 @@ async def cmd_start(message: types.Message):
     await message.answer("Hello! This is bot for Laptop_site administration.")
 
 
+@dp.callback_query(F.data == "answer_question", StateFilter(None))
+async def answer_comment_button(callback: types.CallbackQuery, state: FSMContext):
+    comment_id = get_comment_id_from_message(callback.message.text)
+
+    await callback.message.answer(f"Please send your answer to comment with id {comment_id}")
+    await state.set_state(SendAnswer.sending_answer)
+    await state.update_data(comment_id=comment_id)
+    await callback.answer()
+
+
+@dp.callback_query(F.data == "delete_question", StateFilter(None))
+async def delete_button_handler(callback: types.CallbackQuery, state: FSMContext):
+    comment_id = get_comment_id_from_message(callback.message.text)
+
+    await callback.message.answer(text=f"Are you sure you want to delete comment with id "
+                                       f"{comment_id}?",
+                                  reply_markup=yes_no_keyboard())
+    await state.set_state(ConfirmCommentDeletion.confirm_deletion)
+    await state.update_data(comment_id=comment_id)
+    await callback.answer()
+
+
+@dp.message(SendAnswer.sending_answer)
+async def answer_comment(message: Message, state: FSMContext):
+    state_info = await state.get_data()
+    comment_id = state_info['comment_id']
+
+    if comment_id:
+        try:
+            await save_comment_to_db(comment_id, message.text)
+            await message.reply("Answer was posted", reply_markup=ReplyKeyboardRemove())
+        except ObjectDoesNotExist:
+            await message.answer(f"Comment with id {comment_id} does not exist",
+                                 reply_markup=ReplyKeyboardRemove())
+    else:
+        await message.answer("Something went wrong", reply_markup=ReplyKeyboardRemove())
+    await state.clear()
+
+
+@dp.message(ConfirmCommentDeletion.confirm_deletion, F.text.in_(['Yes', 'No']))
+async def delete_comment_confirm(message: Message, state: FSMContext):
+    state_info = await state.get_data()
+    comment_id = state_info['comment_id']
+
+    if message.text == 'No':
+        await message.answer("Comment was not deleted", reply_markup=ReplyKeyboardRemove())
+    if message.text == "Yes":
+        if comment_id:
+            try:
+                await delete_comment_from_db(comment_id)
+                await message.answer(f"Comment {comment_id} was successfully deleted",
+                                     reply_markup=ReplyKeyboardRemove())
+            except ObjectDoesNotExist:
+                await message.answer(f"Comment with id {comment_id} does not exist",
+                                     reply_markup=ReplyKeyboardRemove())
+        else:
+            await message.answer("Something went wrong", reply_markup=ReplyKeyboardRemove())
+    await state.clear()
+
+
+def yes_no_keyboard():
+    yes = KeyboardButton(text="Yes")
+    no = KeyboardButton(text="No")
+    return ReplyKeyboardMarkup(keyboard=[[yes, no]], resize_keyboard=True)
+
+
+def get_comment_id_from_message(message_text):
+    search_id = re.search(r'.*id: (\d*).*', message_text)
+    if search_id:
+        comment_id = search_id.group(1)
+        return comment_id
+    else:
+        return None
+
+
 @sync_to_async
-def save_comment(comment_id, text):
+def delete_comment_from_db(comment_id):
+    Comment.objects.get(pk=comment_id).delete()
+
+
+@sync_to_async
+def save_comment_to_db(comment_id, text):
     """
     :param comment_id: parent comment id
     :param text: new comment text
@@ -43,7 +137,8 @@ def save_comment(comment_id, text):
     """
     parent_comment = Comment.objects.get(pk=comment_id)
     laptop_id = parent_comment.laptop_id
-    Comment.objects.create(parent_comment_id=parent_comment, user='admin', comment_text=text, laptop_id=laptop_id)
+    user = UserAccount.objects.get(pk=1)
+    Comment.objects.create(parent_comment_id=parent_comment, user=user, comment_text=text, laptop_id=laptop_id)
 
 
 @dp.message(F.reply_to_message)
@@ -53,14 +148,17 @@ async def reply_handler(message: types.Message):
     call the save_comment function to save the comment with the message text to the database
     inform user about result
     """
-    message_text = message.reply_to_message.text
-    comment_id = re.search(r'.*id: (\d*).*', message_text)
+    base_message_text = message.reply_to_message.text
+    comment_id = get_comment_id_from_message(base_message_text)
+
     if comment_id:
-        comment_id = comment_id.group(1)
-        await save_comment(comment_id, message.text)
-        await message.reply("Answer was posted!")
+        try:
+            await save_comment_to_db(comment_id, message.text)
+            await message.reply("Answer was posted!")
+        except ObjectDoesNotExist:
+            await message.reply(f"Comment with id {comment_id} does not exist")
     else:
-        await message.answer("This is not a question. Comment wasn't posted")
+        await message.reply("This is not a comment")
 
 
 async def main():
